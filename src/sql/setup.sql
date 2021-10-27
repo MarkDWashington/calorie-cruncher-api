38ac41fbd3059eb1996a4e1018ee6209f5038303
nmanor01 <nmanor@mtu.edu>
2021-10-27 13:11:30 -0400

    Update User Table
    add name fields

--- src/sql/setup.sql
+++ src/sql/setup.sql
@@ -1,7 +1,9 @@
 CREATE TABLE User (
     email varchar(100) NOT NULL,
     pass varchar(100) NOT NULL,
+    fname varchar(100) NOT NULL,
+    lname varchar(100) NOT NULL,
     PRIMARY KEY (email)
 );
 
-INSERT INTO User VALUES('nmanor@mtu.edu', 'password');
+INSERT INTO User VALUES('nmanor@mtu.edu', 'password', 'Nolan', 'Manor');
